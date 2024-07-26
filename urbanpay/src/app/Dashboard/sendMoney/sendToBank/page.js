11bcca1cc0dd1417d993f0da294faa86d98e58e0
ayodeji <ayodejiakintobi1@gmail.com>
2024-07-26 16:15:18 +0100

    CREATE: RE-FACTORED CODE-BASE

--- urbanpay/src/app/Dashboard/sendMoney/sendToBank/page.js
+++ urbanpay/src/app/Dashboard/sendMoney/sendToBank/page.js
@@ -354,7 +354,7 @@ const page = () => {
                                                 class="text-white card-btn focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                             >
                                                 <a
-                                                    href="verify.html"
+                                                    href="/Dashboard/sendMoney/SendToBank/Verify"
                                                     class="text-white decoration-none text-capitalize"
                                                 >
                                                     Send money
